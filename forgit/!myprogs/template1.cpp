70e0b1ae75ece6b34e59185796910ff66bcc6223
Surajv <vsuraj2405@gmail.com>
2020-08-02 11:00:42 +0530

    added template

---
+++ forgit/!myprogs/template1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#define int long long int
+#define setbits(x) __builtin_popcount(x)
+using namespace std;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main() {
+	c_p_c();
+
+	// code
+
+	return 0;
+}
