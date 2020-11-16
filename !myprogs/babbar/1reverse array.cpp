4b520e9ea83c0cc9ac3c18176caa711dbeb3eb75
Surajv <vsuraj2405@gmail.com>
2020-11-16 17:09:30 +0530

    babbar progs

---
+++ !myprogs/babbar/1reverse array.cpp
@@ -0,0 +1,75 @@
+/*
+///////////////////////////////////////////
+
+
+
+Reverse the array
+
+///////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+#define int long long int
+#define setbits(x) __builtin_popcount(x)
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define ct(x) cout<<x<<endl;
+#define ct2(x,y) cout<<x<<" "<<y<<endl;
+#define forn(i, n) for (int i = 0; i < (int)(n); i++)
+#define forx(i, x, n) for (int i = x ; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+//typedef long long int lli;
+typedef long double ld;
+//typedef pair<int, int> pii;
+//typedef std::vector<int> vi;
+//typedef vector<pii> vii;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+using namespace std;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main() {
+
+///////////
+	c_p_c();
+///////////
+
+	// code
+
+
+	/* Function to reverse arr[] from start to end*/
+	void rvereseArray(int arr[], int start, int end)
+	{
+		while (start < end)
+		{
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+
+	void printArray(int arr[], int size) {} ...
+
+
+
+
+
+
+
+	return 0;
+}
