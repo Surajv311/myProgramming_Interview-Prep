21d61a90a57b34e227a03bd2adf93f187ba6dc27
Surajv <vsuraj2405@gmail.com>
2020-12-29 13:50:31 +0530

    check

--- !myprogs/!template1.cpp
+++ !myprogs/!template1.cpp
@@ -9,23 +9,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int 			long long int
-#define setbits(x) 		__builtin_popcount(x)
-#define pb(x) 			push_back(x)
-#define mp(x,y) 		make_pair(x,y)
-#define ct(x) 			cout<<x<<endl;
-#define ct2(x,y) 		cout<<x<<" "<<y<<endl;
-#define forn(i,n) 		for(int i = 0; i < (int)(n); i++)
-#define forx(i,x,n) 	for(int i = x; i < (int)(n); i++)
-#define all(v) 			v.begin(),v.end()
-#define fsp(x,y) 		fixed<<setprecision(y)<<x;
-#define PI 				3.1415926535897932384626433832795
-#define MOD 			1000000007 // (1e9+7)
-#define pii 			pair<int,int>;
-#define pis 			pair<int,string>;
-#define vi  			vector<int>;
-#define vii 			vector<pii>;
-#define mii 			map<int,int>;
+#define int 		   long long int
+#define setbits(x) 	   __builtin_popcount(x)
+#define pb(x) 		   push_back(x)
+#define mp(x,y) 	   make_pair(x,y)
+#define ct(x) 		   cout<<x<<endl;
+#define ct2(x,y) 	   cout<<x<<" "<<y<<endl;
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); i++)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); i++)
+#define all(v) 		   v.begin(),v.end()
+#define fsp(x,y)       fixed<<setprecision(y)<<x;
+#define PI 			   3.1415926535897932384626433832795
+#define MOD 		   1000000007 // (1e9+7)
+#define pii 		   pair<int,int>;
+#define pis 		   pair<int,string>;
+#define vi  		   vector<int>;
+#define vii 		   vector<pii>;
+#define mii 		   map<int,int>;
 //typedef long long int lli;
 typedef long double ld;
 
