9f650464f3ebd6ec35c102786f3f4bfb7180971e
Surajv <vsuraj2405@gmail.com>
2021-02-05 22:42:27 +0530

    template modified, bigo sheet, feb lc CC

--- !myprogs/codechef/feb'21 long challenge/HDIVISR.cpp
+++ !myprogs/codechef/feb'21 long challenge/HDIVISR.cpp
@@ -1,15 +1,39 @@
 /*
 ///////////////////////////////////////////
 //Question/Info
+You are given an integer N. Find the largest integer between 1 and 10 (inclusive) which divides N.
 
+Input
+The first and only line of the input contains a single integer N.
 
+Output
+Print a single line containing one integer ― the largest divisor of N between 1 and 10.
+
+Constraints
+2≤N≤1,000
+Subtasks
+Subtask #1 (100 points): original constraints
+
+Example Input 1
+24
+Example Output 1
+8
+Explanation
+The divisors of 24 are 1,2,3,4,6,8,12,24, out of which 1,2,3,4,6,8 are in the range [1,10]. Therefore, the answer is max(1,2,3,4,6,8)=8.
+
+Example Input 2
+91
+Example Output 2
+7
+Explanation
+The divisors of 91 are 1,7,13,91, out of which only 1 and 7 are in the range [1,10]. Therefore, the answer is max(1,7)=7.
 ///////////////////////////////////////////
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int 		   long long int
+// #define int 		   long long int
 #define setbits(x) 	   __builtin_popcount(x)
 #define pb(x) 		   push_back(x)
 #define mp(x,y) 	   make_pair(x,y)
@@ -28,7 +52,7 @@ using namespace std;
 #define vii 		   vector<pii>
 #define mii 		   map<int,int>
 
-// typedef long long int lli;
+typedef long long int lli;
 typedef long double ld;
 
 #pragma GCC optimize("Ofast")
@@ -54,9 +78,18 @@ int32_t main() {
 	/*
 	int t ; cin >> t; while(t--){}
 	*/
+	int n ;
+	cin >> n ;
 
+	int ans = 0;
+	for (int i = 10; i > 0; i--) {
+		if (n % i == 0) {
+			ans = i;
+			break;
+		}
+	}
 
-
+	ct(ans)
 
 
 // cerr << "time: " << clock() << " ms" << '\n';
