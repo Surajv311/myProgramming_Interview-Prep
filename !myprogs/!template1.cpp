519cfb221791b39dfd189eb2f51381472a551b96
Surajv <vsuraj2405@gmail.com>
2021-02-13 22:41:14 +0530

    template update

--- !myprogs/!template1.cpp
+++ !myprogs/!template1.cpp
@@ -3,6 +3,7 @@
 //Question/Info
 
 
+
 ///////////////////////////////////////////
 */
 
@@ -14,12 +15,13 @@ using namespace std;
 #define pb(x) 		   push_back(x)
 #define mp(x,y) 	   make_pair(x,y)
 #define eb(x) 		   emplace_back(x)
-#define ct(x) 		   cout<<x<<endl;
-#define ct2(x,y) 	   cout<<x<<" "<<y<<endl;
-#define forn(i,n) 	   for(int i = 0; i < (int)(n); i++)
-#define forx(i,x,n)    for(int i = x; i < (int)(n); i++)
+#define ct(x) 		   cout << x << endl;
+#define ct2(x,y) 	   cout << x << " " << y << endl;
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); ++i)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); ++i)
+#define nfor(i,n)      for(int i = n-1; i >= 0; --i)
 #define all(v) 		   v.begin(),v.end()
-#define fsp(x,y)       fixed<<setprecision(y)<<x;
+#define fsp(x,y)       fixed << setprecision(y) << x;
 #define PI 			   3.1415926535897932384626433832795
 #define MOD 		   1000000007 // (1e9+7)
 #define pii 		   pair<int,int>
@@ -27,13 +29,12 @@ using namespace std;
 #define vi  		   vector<int>
 #define vii 		   vector<pii>
 #define mii 		   map<int,int>
+#define p_q            priority_queue // priority_queue<int> (&) priority_queue< int,vi,greater<int> >
 #define _IOS      	   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-// typedef long long int lli;
 typedef long double ld;
 
 #pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
 
 void c_p_c()
 {
@@ -55,6 +56,7 @@ int32_t main() {
 	/*
 	int t ; cin >> t; while(t--){}
 	*/
+
 
 
 
