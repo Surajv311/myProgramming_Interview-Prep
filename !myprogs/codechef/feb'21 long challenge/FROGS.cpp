850fe0387551403ee8b7ddc46ed5062e022d8780
Surajv <vsuraj2405@gmail.com>
2021-02-14 00:15:00 +0530

    added q

--- !myprogs/codechef/feb'21 long challenge/FROGS.cpp
+++ !myprogs/codechef/feb'21 long challenge/FROGS.cpp
@@ -2,7 +2,52 @@
 ///////////////////////////////////////////
 //Question/Info
 
+There are N frogs (numbered 1 through N) in a line. For each valid i, the i-th frog is initially at the position i, it has weight Wi, and whenever you hit its back, it jumps a distance Li to the right, i.e. its position increases by Li. The weights of the frogs are pairwise distinct.
 
+You can hit the back of each frog any number of times (possibly zero, not necessarily the same for all frogs) in any order. The frogs do not intefere with each other, so there can be any number of frogs at the same time at each position.
+
+Your task is to sort the frogs in the increasing order of weight using the smallest possible number of hits. In other words, after all the hits are performed, then for each pair of frogs (i,j) such that Wi<Wj, the position of the i-th frog should be strictly smaller than the position of the j-th frog. Find the smallest number of hits needed to achieve such a state.
+
+Input
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains a single integer N.
+The second line contains N space-separated integers W1,W2,…,WN.
+The third line contains N space-separated integers L1,L2,…,LN.
+Output
+For each test case, print a single line containing one integer ― the smallest number of times you need to hit the backs of the frogs.
+
+Constraints
+1≤T≤2⋅104
+2≤N≤4
+1≤Wi≤N for each valid i
+1≤Li≤5 for each valid i
+no two frogs have the same weight
+Subtasks
+Subtask #1 (50 points):
+
+T=50
+N=2
+Subtask #2 (50 points): original constraints
+
+Example Input
+3
+3
+3 1 2
+1 4 5
+3
+3 2 1
+1 1 1
+4
+2 1 4 3
+4 1 2 4
+Example Output
+3
+6
+5
+Explanation
+Example case 1: We can hit the back of the first frog three times.
+
+Example case 2: We can hit the back of the first frog four times, then hit the back of the second frog two times.
 ///////////////////////////////////////////
 */
 
@@ -190,7 +235,7 @@ int ffour(int n, int wt[], int jmp[]) {
 
 int32_t main() {
 ///////////
-//	c_p_c();
+	c_p_c();
 ///////////
 	_IOS
 //////////
--- !myprogs/codechef/feb'21 long challenge/TEAMNAME.cpp
+++ !myprogs/codechef/feb'21 long challenge/TEAMNAME.cpp
@@ -2,7 +2,49 @@
 ///////////////////////////////////////////
 //Question/Info
 
+Сhef has assembled a football team! Now he needs to choose a name for his team. There are N words in English that Chef considers funny. These words are s1,s2,…,sN.
 
+Chef thinks that a good team name should consist of two words such that they are not funny, but if we swap the first letters in them, the resulting words are funny. Note that under the given constraints, this means that a word is a non-empty string containing only lowercase English letters.
+
+Can you tell Chef how many good team names there are?
+
+Input
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains a single integer N.
+The second line contains N space-separated strings s1,s2,…,sN.
+Output
+For each test case, print a single line containing one integer — the number of ways to choose a good team name.
+
+Constraints
+1≤T≤100
+2≤N≤2⋅104
+2≤|si|≤20 for each valid i
+s1,s2,…,sN contain only lowercase English letters
+s1,s2,…,sN are pairwise distinct
+the sum of N over all test cases does not exceed 2⋅104
+Subtasks
+Subtask #1 (20 points): s1,s2,…,sN contain only letters 'a' and 'b'
+
+Subtask #2 (80 points): original constraints
+
+Example Input
+3
+2
+suf mas
+3
+good game guys
+4
+hell bell best test
+Example Output
+2
+0
+2
+Explanation
+Example case 1: The good team names are ("muf", "sas") and ("sas", "muf").
+
+Example case 2: There are no good team names because all funny words start with 'g'.
+
+Example case 3: The good team names are ("hest", "tell") and ("tell", "hest").
 ///////////////////////////////////////////
 */
 
@@ -90,7 +132,7 @@ int compute(map<string, vector<char>> &adj )
 
 int32_t main() {
 ///////////
-//	c_p_c();
+	c_p_c();
 ///////////
 	_IOS
 //////////
