e1cca12576f0bfb7705eba5bab4f938ab6550c25
Surajv <vsuraj2405@gmail.com>
2021-03-14 16:47:55 +0530

    added misc /

---
+++ !myprogs/averma/heap/MISC.cpp
@@ -0,0 +1,19 @@
+/*
+
+try to think of heap whenever smallest/largest asked in question...
+use min heap to get ~largest ... and max heap for ~smallest...
+
+--------------------------------------------------------
+
+ Return K largest Elements in array
+
+ Use minheap... since in min heap the element at top
+ would be the smallest and the ones below would
+ be larger than that ...push elements in it and then
+ pop elements from it when heap size > k ...
+
+ -------------------------------------------------------
+
+
+
+*/
