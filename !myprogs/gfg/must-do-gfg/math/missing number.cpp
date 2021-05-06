10f6567c94ebc14c41baaf59ac082ad2cfe5c046
Surajv <vsuraj2405@gmail.com>
2021-05-06 21:32:36 +0530

    easy progs .. code...

---
+++ !myprogs/gfg/must-do-gfg/math/missing number.cpp
@@ -0,0 +1,113 @@
+/*
+///////////////////////////////////////////
+//Question/Info
+
+Missing number in array
+Easy Accuracy: 42.51% Submissions: 68762 Points: 2
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {1,2,3,4,5,6,7,8,10}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+
+Company Tags
+ Accolite Adobe Amazon Cisco D-E-Shaw Intuit Microsoft Morgan Stanley Ola Cabs Payu Qualcomm Visa
+
+author: srj_v
+///////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int 		   long long int
+#define sbit(x) 	   __builtin_popcount(x)
+#define pb(x) 		   push_back(x)
+#define mp(x,y) 	   make_pair(x,y)
+#define eb(x) 		   emplace_back(x)
+#define ct(x) 		   cout << x << "\n";
+#define ct2(x,y) 	   cout << x << " " << y << "\n";
+#define tc(x) 		   cout << x << " ";
+#define tc2(x,y) 	   cout << x << " " << y << " ";
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); ++i)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); ++i)
+#define nfor(i,n)      for(int i = n-1; i >= 0; --i)
+#define all(v) 		   v.begin(),v.end()
+#define fsp(x,y)       fixed << setprecision(y) << x
+#define PI 			   3.1415926535897932384626433832795
+#define MOD 		   1000000007 // (1e9+7)
+#define pii 		   pair<int,int>
+#define pis 		   pair<int,string>
+#define vi  		   vector<int>
+#define vii 		   vector<pii>
+#define mii 		   map<int,int>
+#define p_q            priority_queue // priority_queue<int> (&) priority_queue< int,vi,greater<int> >
+#define _IOS      	   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long double ld;
+typedef long long int lli;
+
+#pragma GCC optimize("Ofast")
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main() {
+///////////
+	c_p_c();
+///////////
+	_IOS
+//////////
+
+	// code
+
+	/*
+	int t ; cin >> t; while(t--){}
+	*/
+
+
+	class Solution {
+	public:
+		int MissingNumber(vector<int>& array, int n) {
+			// Your code goes here
+
+			int sum = (n) * (n + 1) / 2;
+
+			int dif = accumulate(array.begin(), array.end(), 0);
+
+			return sum - dif;
+		}
+	};
+
+
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
---
+++ !myprogs/gfg/must-do-gfg/math/trailing zeroes.cpp
@@ -0,0 +1,116 @@
+/*
+///////////////////////////////////////////
+//Question/Info
+
+Trailing zeroes in factorial
+Easy Accuracy: 52.7% Submissions: 12330 Points: 2
+For an integer N find the number of trailing zeroes in N!.
+
+Example 1:
+
+Input:
+N = 5
+Output:
+1
+Explanation:
+5! = 120 so the number of trailing zero is 1.
+Example 2:
+
+Input:
+N = 4
+Output:
+0
+Explanation:
+4! = 24 so the number of trailing zero is 0.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function trailingZeroes() which take an integer N as an input parameter and returns the count of trailing zeroes in the N!.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 109
+
+Company Tags
+ MakeMyTrip Microsoft
+
+author: srj_v
+///////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int 		   long long int
+#define sbit(x) 	   __builtin_popcount(x)
+#define pb(x) 		   push_back(x)
+#define mp(x,y) 	   make_pair(x,y)
+#define eb(x) 		   emplace_back(x)
+#define ct(x) 		   cout << x << "\n";
+#define ct2(x,y) 	   cout << x << " " << y << "\n";
+#define tc(x) 		   cout << x << " ";
+#define tc2(x,y) 	   cout << x << " " << y << " ";
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); ++i)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); ++i)
+#define nfor(i,n)      for(int i = n-1; i >= 0; --i)
+#define all(v) 		   v.begin(),v.end()
+#define fsp(x,y)       fixed << setprecision(y) << x
+#define PI 			   3.1415926535897932384626433832795
+#define MOD 		   1000000007 // (1e9+7)
+#define pii 		   pair<int,int>
+#define pis 		   pair<int,string>
+#define vi  		   vector<int>
+#define vii 		   vector<pii>
+#define mii 		   map<int,int>
+#define p_q            priority_queue // priority_queue<int> (&) priority_queue< int,vi,greater<int> >
+#define _IOS      	   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long double ld;
+typedef long long int lli;
+
+#pragma GCC optimize("Ofast")
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main() {
+///////////
+	c_p_c();
+///////////
+	_IOS
+//////////
+
+	// code
+
+	/*
+	int t ; cin >> t; while(t--){}
+	*/
+
+	class Solution
+	{
+	public:
+		int trailingZeroes(int N)
+		{
+			// Write Your Code here
+			int ans = 0;
+			for (int i = 1 ; N / (pow(5, i)) > 0; ++i )
+			{
+				ans += N / (pow(5, i));
+			}
+
+			return ans;
+		}
+	};
+
+
+
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
