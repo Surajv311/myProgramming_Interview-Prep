cf058b257cf2f615d58c733ae3a860c493f2720f
Surajv <vsuraj2405@gmail.com>
2021-05-09 13:28:28 +0530

    anagram code...

---
+++ !myprogs/gfg/must-do-gfg/string/anagram.cpp
@@ -0,0 +1,123 @@
+/*
+///////////////////////////////////////////
+//Question/Info
+
+Anagram
+Easy Accuracy: 50.99% Submissions: 27234 Points: 2
+Given two strings a and b consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, “act” and “tac” are an anagram of each other.
+
+Example 1:
+
+Input:
+a = geeksforgeeks, b = forgeeksgeeks
+Output: YES
+Explanation: Both the string have same
+characters with same frequency. So,
+both are anagrams.
+Example 2:
+
+Input:
+a = allergy, b = allergic
+Output: NO
+Explanation:Characters in both the strings
+are not same, so they are not anagrams.
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function isAnagram() which takes the string a and string b as input parameter and check if the two strings are an anagram of each other. The function returns true if the strings are anagram else it returns false.
+
+Expected Time Complexity: O(|a|+|b|).
+Expected Auxiliary Space: O(Number of distinct characters).
+
+Note: |s| represents the length of string s.
+
+Constraints:
+1 ≤ |a|,|b| ≤ 105
+
+Company Tags
+ Nagarro Media.net Directi
+
+author: srj_v
+///////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int 		   long long int
+#define sbit(x) 	   __builtin_popcount(x)
+#define pb(x) 		   push_back(x)
+#define mp(x,y) 	   make_pair(x,y)
+#define eb(x) 		   emplace_back(x)
+#define ct(x) 		   cout << x << "\n";
+#define ct2(x,y) 	   cout << x << " " << y << "\n";
+#define tc(x) 		   cout << x << " ";
+#define tc2(x,y) 	   cout << x << " " << y << " ";
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); ++i)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); ++i)
+#define nfor(i,n)      for(int i = n-1; i >= 0; --i)
+#define all(v) 		   v.begin(),v.end()
+#define fsp(x,y)       fixed << setprecision(y) << x
+#define PI 			   3.1415926535897932384626433832795
+#define MOD 		   1000000007 // (1e9+7)
+#define pii 		   pair<int,int>
+#define pis 		   pair<int,string>
+#define vi  		   vector<int>
+#define vii 		   vector<pii>
+#define mii 		   map<int,int>
+#define p_q            priority_queue // priority_queue<int> (&) priority_queue< int,vi,greater<int> >
+#define _IOS      	   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long double ld;
+typedef long long int lli;
+
+#pragma GCC optimize("Ofast")
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main() {
+///////////
+	c_p_c();
+///////////
+	_IOS
+//////////
+
+	// code
+
+	/*
+	int t ; cin >> t; while(t--){}
+	*/
+
+
+	class Solution
+	{
+	public:
+		//Function is to check whether two strings are anagram of each other or not.
+		bool isAnagram(string a, string b) {
+
+			// Your code here
+
+			sort(a.begin(), a.end());
+			sort(b.begin(), b.end());
+
+			if (a == b) {
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+
+	};
+
+
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
