f41a7ad76db1ad06f3e7e56f195371ea7c0cc2b4
Surajv <vsuraj2405@gmail.com>
2021-05-13 23:43:04 +0530

    easy q's....

---
+++ !myprogs/gfg/must-do-gfg/arrays/chocolate distribution problem.cpp
@@ -0,0 +1,136 @@
+/*
+///////////////////////////////////////////
+//Question/Info
+
+Chocolate Distribution Problem
+Easy Accuracy: 67.69% Submissions: 6470 Points: 2
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between
+maximum chocolates and minimum chocolates
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+Example 2:
+
+Input:
+N = 7, M = 3
+A = {7, 3, 2, 4, 9, 12, 56}
+Output: 2
+Explanation: The minimum difference between
+maximum chocolates and minimum chocolates
+is 4 - 2 = 2 by choosing following M packets :
+{3, 2, 4}.
+Your Task:
+You don't need to take any input or print anything. Your task is to complete the function findMinDiff() which takes array A[ ], N and M as input parameters and returns the minimum possible difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student.
+
+Expected Time Complexity: O(N*Log(N))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 105
+1 ≤ Ai ≤ 109
+1 ≤ M ≤ N
+
+Company Tags
+ Flipkart
+
+author: srj_v
+///////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int 		   long long int
+#define sbit(x) 	   __builtin_popcount(x)
+#define pb(x) 		   push_back(x)
+#define mp(x,y) 	   make_pair(x,y)
+#define eb(x) 		   emplace_back(x)
+#define ct(x) 		   cout << x << "\n";
+#define ct2(x,y) 	   cout << x << " " << y << "\n";
+#define tc(x) 		   cout << x << " ";
+#define tc2(x,y) 	   cout << x << " " << y << " ";
+#define forn(i,n) 	   for(int i = 0; i < (int)(n); ++i)
+#define forx(i,x,n)    for(int i = x; i < (int)(n); ++i)
+#define nfor(i,n)      for(int i = n-1; i >= 0; --i)
+#define all(v) 		   v.begin(),v.end()
+#define fsp(x,y)       fixed << setprecision(y) << x
+#define PI 			   3.1415926535897932384626433832795
+#define MOD 		   1000000007 // (1e9+7)
+#define pii 		   pair<int,int>
+#define pis 		   pair<int,string>
+#define vi  		   vector<int>
+#define vii 		   vector<pii>
+#define mii 		   map<int,int>
+#define p_q            priority_queue // priority_queue<int> (&) priority_queue< int,vi,greater<int> >
+#define _IOS      	   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long double ld;
+typedef long long int lli;
+
+#pragma GCC optimize("Ofast")
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int32_t main() {
+///////////
+	c_p_c();
+///////////
+	_IOS
+//////////
+
+	// code
+
+	/*
+	int t ; cin >> t; while(t--){}
+	*/
+
+
+	class Solution {
+	public:
+		long long findMinDiff(vector<long long> arr, long long n, long long m) {
+			//code
+
+			sort(arr.begin(), arr.end());
+
+			if (m > n) {
+				return -1;
+			}
+
+			int ans = INT_MAX;
+
+			for (int i = 0 ;  i + m - 1 < n; i++) {
+
+				int x = arr[i + m - 1] - arr[i];
+
+				ans = min(ans, x);
+
+			}
+
+			return ans;
+
+		}
+	};
+
+
+
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
