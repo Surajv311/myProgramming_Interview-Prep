6ab3ad153623385948046b64399b152910ede604
Surajv <vsuraj2405@gmail.com>
2021-07-01 14:58:03 +0530

    question improved

--- !myprogs/gfg/must-do-gfg/tree/other_binary-search-tree-qs/populate next right pointers.cpp
+++ !myprogs/gfg/must-do-gfg/tree/other_binary-search-tree-qs/populate next right pointers.cpp
@@ -46,6 +46,52 @@ Constraints:
 The number of nodes in the given tree is less than 4096.
 -1000 <= node.val <= 1000
 
+GFG EXPLANATION FOR Q...
+
+Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.
+
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
+       10                       10 ------> NULL
+      / \                       /      \
+     3   5       =>     3 ------> 5 --------> NULL
+    / \     \               /  \           \
+   4   1   2          4 --> 1 -----> 2 -------> NULL
+
+
+
+Example 1:
+
+Input:
+     3
+   /  \
+  1    2
+Output:
+3 1 2
+1 3 2
+Explanation:The connected tree is
+        3 ------> NULL
+     /    \
+    1-----> 2 ------ NULL
+Example 2:
+
+Input:
+      10
+    /   \
+   20   30
+  /  \
+ 40  60
+Output:
+10 20 30 40 60
+40 20 60 10 30
+Explanation:The connected tree is
+         10 ----------> NULL
+       /     \
+     20 ------> 30 -------> NULL
+  /    \
+ 40 ----> 60 ----------> NULL
+
+
+
 author: srj_v
 ///////////////////////////////////////////
 */
