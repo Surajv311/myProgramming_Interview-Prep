9ca3d4073801d4931be26c74b04f6bb373f4d4fe
Surajv <vsuraj2405@gmail.com>
2021-07-16 23:40:23 +0530

    anagram

---
+++ !myprogs/gfg/must-do-gfg/string/other/anagram.cpp
@@ -0,0 +1,103 @@
+/*
+//////////////////////////////////////////////////////
+//Question/Info
+
+Anagram
+Easy Accuracy: 50.99% Submissions: 39140 Points: 2
+Given two strings a and b consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, “act” and “tac” are an anagram of each other.
+
+Example 1:
+
+Input:
+a = geeksforgeeks, b = forgeeksgeeks
+Output: YES
+Explanation: Both the string have same
+characters with same frequency. So,
+both are anagrams.
+Example 2:
+
+Input:
+a = allergy, b = allergic
+Output: NO
+Explanation:Characters in both the strings
+are not same, so they are not anagrams.
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function isAnagram() which takes the string a and string b as input parameter and check if the two strings are an anagram of each other. The function returns true if the strings are anagram else it returns false.
+
+Expected Time Complexity: O(|a|+|b|).
+Expected Auxiliary Space: O(Number of distinct characters).
+
+Note: |s| represents the length of string s.
+
+Constraints:
+1 ≤ |a|,|b| ≤ 105
+
+Company Tags
+ Nagarro Media.net Directi Google
+
+author: srj_v
+//////////////////////////////////////////////////////
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _IOS   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long double ld;
+typedef long long int lli;
+
+#pragma GCC optimize("Ofast")
+
+void c_p_c()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+//////////////////////////////////////////////////////
+
+
+int32_t main() {
+///////////
+//	c_p_c();
+///////////
+	_IOS
+//////////
+
+	// code
+
+	/*
+	int t ; cin >> t; while(t--){}
+	*/
+
+
+	bool isAnagram(string a, string b) {
+
+		// Your code here
+
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end());
+		if (a == b) return true;
+		else return false;
+
+		/*
+
+		two methods:
+		1- sort the strings and compare...o(nlogn)
+		2- maintain the character array ...increment if chars found...and then compare the array.
+
+		*/
+
+	}
+
+
+
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
+
+//////////////////////////////////////////////////////
