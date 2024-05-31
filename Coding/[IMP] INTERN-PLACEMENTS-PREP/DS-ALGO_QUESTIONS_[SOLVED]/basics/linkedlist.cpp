1cbdb200fd3fa6c9b1aa802d4f724f33441e7cfd
Suraj Verma <59371846+Surajv311@users.noreply.github.com>
2024-06-01 00:27:13 +0530

    Update linkedlist.cpp

--- Coding/[IMP] INTERN-PLACEMENTS-PREP/DS-ALGO_QUESTIONS_[SOLVED]/basics/linkedlist.cpp
+++ Coding/[IMP] INTERN-PLACEMENTS-PREP/DS-ALGO_QUESTIONS_[SOLVED]/basics/linkedlist.cpp
@@ -1,1 +1,41 @@
 
+class Node {
+public:
+	int data;
+	Node* next;
+	Node(int data) {
+		this->data = data;
+		next = 0;
+	}
+};
+
+Node* adddata(Node*&node, int data)
+{
+	Node* newNode = new Node(data);
+	node->next = newNode;
+	return newNode;
+
+}
+
+int32_t main() {
+///////////
+//	c_p_c();
+///////////
+	_IOS
+//////////
+
+	Node* root = new Node(10);// root node
+	Node* node = root;
+
+	int n = 5;
+	for (int i = 0 ; i < n ; i++) {
+		int data; cin >> data;
+		node = adddata(node, data);
+	}
+
+	// traversal can be done using while loop
+
+// cerr << "time: " << clock() << " ms" << '\n';
+	return 0;
+}
+
